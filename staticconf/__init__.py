d8771d7cf5c2aa6c44a222129a0948a266ec110e
Daniel Nephin <dnephin@yelp.com>
2012-12-10 13:06:07 -0800

    Bump version to 0.4.7

--- staticconf/__init__.py
+++ staticconf/__init__.py
@@ -3,7 +3,7 @@ from staticconf.loader import *
 from staticconf.getters import *
 
 
-version         = "0.4.6"
+version         = "0.4.7"
 
 view_help       = config.view_help
 reload          = config.reload
