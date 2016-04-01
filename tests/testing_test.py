c4b0d38b0eff685b86a771622b31aa9394027a90
Eliot Alter <eliot@yelp.com>
2016-04-01 11:04:13 -0700

    Add new testing function UpdateConfiguration

--- tests/testing_test.py
+++ tests/testing_test.py
@@ -22,3 +22,20 @@ class TestMockConfiguration(object):
         with testing.MockConfiguration(conf):
             assert_equal(staticconf.get('a.b'), 'two')
             assert_equal(staticconf.get('c'), 'three')
+
+
+class TestPatchConfiguration(object):
+
+    def test_nested(self):
+        with testing.MockConfiguration(a='one', b='two'):
+            with testing.PatchConfiguration(a='three'):
+                assert_equal(staticconf.get('a'), 'three')
+                assert_equal(staticconf.get('b'), 'two')
+
+            assert_equal(staticconf.get('a'), 'one')
+            assert_equal(staticconf.get('b'), 'two')
+
+    def test_not_nested(self):
+        with testing.PatchConfiguration(a='one', b='two'):
+            assert_equal(staticconf.get('a'), 'one')
+            assert_equal(staticconf.get('b'), 'two')
